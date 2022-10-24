bb99e428946a2410c4a3f6fb2a45806c117ac9d6
Arthur Béquié <arthurbequie@protonmail.com>
2022-10-25 01:08:19 +0200

    wip

---
+++ api/lib/api/timescale/working_time.ex
@@ -0,0 +1,19 @@
+defmodule Api.Timescale.WorkingTime do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "working_times" do
+    field :end, :naive_datetime
+    field :start, :naive_datetime
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(working_time, attrs) do
+    working_time
+    |> cast(attrs, [:start, :end])
+    |> validate_required([:start, :end])
+  end
+end
--- api/lib/api_web/router.ex
+++ api/lib/api_web/router.ex
@@ -1,11 +1,56 @@
 defmodule ApiWeb.Router do
   use ApiWeb, :router
 
+  import ApiWeb.UserAuth
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {ApiWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :fetch_current_user
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/api", ApiWeb do
     pipe_through :api
+  end
+
+  ## Authentication routes
+
+  scope "/", ApiWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", ApiWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", ApiWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :edit
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
